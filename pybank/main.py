05e41c11bb7e931f6f0a3fc6e433fc52fb4a4231
Kevin McCurdy <35277808+khmccurdy@users.noreply.github.com>
2018-02-17 21:35:24 -0800

    Update pybank with comments, add pypoll

--- pybank/main.py
+++ pybank/main.py
@@ -1,10 +1,10 @@
 import os
 import csv
 
+#create numbered list of csv files to choose from at runtime
 csvfolder = 'raw_data'
 docs = os.listdir(csvfolder)
 csvfilelist = [doc for doc in docs if doc.endswith(".csv")]
-#csvoptions = ["("+str(i)+") "+csvfilelist[i] for i in range(len(csvfilelist))]
 csvoptions = ""
 for i in range(len(csvfilelist)):
 	csvoptions += " ("+str(i)+") "+ csvfilelist[i]+" "
@@ -14,11 +14,13 @@ print(csvoptions)
 filenum = int(input("Read file number: "))
 filename = csvfilelist[filenum]
 
+#open selected csv file
 csvpath = os.path.join(csvfolder, filename)
 
 with open(csvpath, newline = '') as csvfile:
 	csvreader = csv.reader(csvfile, delimiter=',')
 	
+	#initialize variables
 	months=0
 	totalRev = 0
 	firstRev = 0
@@ -27,25 +29,32 @@ with open(csvpath, newline = '') as csvfile:
 	minrow = ["No Decrease", 0]
 	
 	for row in csvreader:
+		#don't count the label row
+		
 		if row[0] == "Date":
 			continue
 		months += 1
 		
+		#convert revenue column to an integer, sum values
 		rev = int(row[1])
 		totalRev += rev
 		
+		#calculate running max increase and decrease
 		if rev > maxrow[1]:
 			maxrow = [row[0], rev]
 		elif rev < minrow[1]:
 			minrow = [row[0], rev]
 		
+		#store first and final revenues for use in average change
 		if months==1:
 			firstRev = rev
 		lastRev = rev
 
+#calculate average change
 avgChange = (lastRev-firstRev)/(months-1)
 avgChange = round(avgChange)
 
+#save output lines to a list
 analysis = ["\nFinancial Analysis \n-------------------",
 			"Total Months: "+ str(months),
 			"Total Revenue: $" + str(totalRev),
@@ -53,6 +62,7 @@ analysis = ["\nFinancial Analysis \n-------------------",
 			"Greatest Increase in Revenue: " + maxrow[0] + " ($"+str(maxrow[1])+")",
 			"Greatest Decrease in Revenue: " + minrow[0] + " ($"+str(minrow[1])+")"]
 
+#write output to file and print to terminal
 with open("analysis.txt","w") as output:
 	for line in analysis:
 		output.write(line+"\n")
---
+++ pypoll/main.py
@@ -0,0 +1,67 @@
+import os
+import csv
+
+#create numbered list of csv files to choose from at runtime
+csvfolder = 'raw_data'
+docs = os.listdir(csvfolder)
+csvfilelist = [doc for doc in docs if doc.endswith(".csv")]
+csvoptions = ""
+for i in range(len(csvfilelist)):
+	csvoptions += " ("+str(i)+") "+ csvfilelist[i]+" "
+
+print("Choose a file:")
+print(csvoptions)
+filenum = int(input("Read file number: "))
+filename = csvfilelist[filenum]
+
+#open selected csv file
+csvpath = os.path.join(csvfolder, filename)
+
+with open(csvpath, newline = '') as csvfile:
+	csvreader = csv.reader(csvfile, delimiter=',')
+	
+	#initialize variables
+	votes = 0
+	candidates = {}
+	
+	for row in csvreader:
+		if row[0] == "Voter ID":
+			continue
+		votes += 1
+		
+		#if a candidate has not been listed, at them to the dictionary
+		#otherwise, increment their vote count
+		if not row[2] in candidates:
+			candidates[row[2]] = 1
+		else:
+			candidates[row[2]] += 1
+
+#convert dictionary to list
+canList = candidates.items()
+
+#initialize variables to calculate winner
+winner = ""
+maxvotes = 0
+
+#function for formatting percents
+def perc(n):
+	return str(round(n*100,1))+"%"
+
+hline = "-------------------"
+analysis = ["\nElection results", hline,
+			"Total Votes: "+str(votes), hline]
+
+#add candidates to analysis, calculate winner
+for row in canList:
+	analysis.append(row[0]+": " + perc(row[1]/votes)+" ("+ str(row[1])+")")
+	if row[1] > maxvotes:
+		maxvotes = row[1]
+		winner = row[0]
+
+#add winner to analysis
+analysis.extend([hline, "Winner: "+winner, hline])
+
+with open("analysis.txt","w") as output:
+	for line in analysis:
+		output.write(line+"\n")
+		print(line)
